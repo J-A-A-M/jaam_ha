6bb6511cc2d43aefdfe2fa24fcd728914e02473f
Vladyslav Heneraliuk <vladyslav.heneraliuk@gmail.com>
2026-03-10 09:03:40 +0000

    feat(select): add display mode entity
    - Create new display_mode select entity with 6 options
    - Add async_set_display_mode() method in API client
    - Options: off, clock, weather, technical info, microclimate, combined
    - Add Ukrainian and English translations

--- custom_components/jaam_ha/select/__init__.py
+++ custom_components/jaam_ha/select/__init__.py
@@ -7,6 +7,7 @@ from typing import TYPE_CHECKING
 from custom_components.jaam_ha.const import PARALLEL_UPDATES as PARALLEL_UPDATES
 from homeassistant.components.select import SelectEntityDescription
 
+from .display_mode import ENTITY_DESCRIPTIONS as DISPLAY_MODE_DESCRIPTIONS, JaamHADisplayModeSelect
 from .map_mode import ENTITY_DESCRIPTIONS as MAP_MODE_DESCRIPTIONS, JaamHAMapModeSelect
 
 if TYPE_CHECKING:
@@ -15,7 +16,10 @@ if TYPE_CHECKING:
     from homeassistant.helpers.entity_platform import AddEntitiesCallback
 
 # Combine all entity descriptions from different modules
-ENTITY_DESCRIPTIONS: tuple[SelectEntityDescription, ...] = (*MAP_MODE_DESCRIPTIONS,)
+ENTITY_DESCRIPTIONS: tuple[SelectEntityDescription, ...] = (
+    *MAP_MODE_DESCRIPTIONS,
+    *DISPLAY_MODE_DESCRIPTIONS,
+)
 
 
 async def async_setup_entry(
@@ -32,4 +36,13 @@ async def async_setup_entry(
             entity_description=entity_description,
         )
         for entity_description in MAP_MODE_DESCRIPTIONS
+    )
+
+    # Add display mode select
+    async_add_entities(
+        JaamHADisplayModeSelect(
+            coordinator=entry.runtime_data.coordinator,
+            entity_description=entity_description,
+        )
+        for entity_description in DISPLAY_MODE_DESCRIPTIONS
     )
